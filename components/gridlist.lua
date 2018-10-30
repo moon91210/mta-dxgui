479cc2f6551f9d092cf2e5247da304ac98c6edbb
moon91210 <smulders@protonmail.com>
2018-10-30 03:29:30 +0100

    complete overhaul: optimizations and new functions, better column autosizing, reworked thumbnail handling

--- components/gridlist.lua
+++ components/gridlist.lua
@@ -8,7 +8,7 @@ function Gridlist.new(x, y, w, h)
 	self.columns = {}
 	self.items = {}
 	self.itemh = 45
-	self.itemSpace = 2
+	self.itemSpacing = 2
 	self.maxItems = nil
 	self.sp = 1
 	self.ep = nil
@@ -16,80 +16,127 @@ function Gridlist.new(x, y, w, h)
 	self.rt = DxRenderTarget(self.w, self.h, true)
 	self.rt_updated = false
 	self.scrollbarWidth = 15
+	self.paddingLeft = 6
+	self.scrollbarVisible = true
+	self.textSize = 1.3
+	self.titleSpacing = 27
+	self.autoSizeColumn = true
 	return self
 end
 
 local function updateRT(self)
 	dxSetRenderTarget(self.rt, true)
+	self:fitItemsToColumns()
 	self:drawItems()
 	dxSetRenderTarget()
 end
 
-local function resizeColumn(column, val)
-	if (column.autosize) then
-		local textWidth = type(val) == "number" and val or dxGetTextWidth(val, 1.5)
-		if (textWidth > column.width) then
-			column.width = textWidth + 10
+local function parseItemValues(self, values, parseSingle)
+	values = type(values) == 'table' and values or {values}
+	local parsed = {}
+	for i=1, #values do
+		local val = {}
+		val.value = tostring(values[i])
+		val.width = dxGetTextWidth(val.value, self.textSize)
+
+		if self.columns[i].checkThumbnails then
+			if not fileExists(val.value) then
+				val.value = 'img/broken.png'
+			end
+			val.width = self.itemh
+		end
+
+		table.insert(parsed, val)
+	end
+	return parseSingle and parsed[1] or parsed
+end
+
+function Gridlist:addColumn(title, width)
+	check('s', {title})
+
+	local col = {
+		autosize = not width,
+		value = tostring(title),
+		titleWidth = dxGetTextWidth(title, self.textSize),
+		checkThumbnails = false
+	}
+	col.width = width or col.titleWidth
+	table.insert(self.columns, col)
+	return col
+end
+
+function Gridlist:removeColumn(title)
+	check('s', {title})
+
+	for i=#self.columns, 1, -1 do
+		if self.columns[i].value == title then
+			table.remove(self.columns, i)
+		end
+	end	
+end
+
+function Gridlist:setColumnCheckThumbnails(colIndex, state)
+	check('b', {state})
+
+	local col = self.columns[colIndex]
+	col.checkThumbnails = state
+	if state then
+		for i=1, #self.items do
+			self:validateItemPath(i, colIndex)
 		end
 	end
 end
 
-function Gridlist:addColumn(val, width)
-	local column = {}
-
-	column.autosize = not width
-	column.val = val
-	column.width = width or dxGetTextWidth(val, 1.5) + 10
-	--column.btn = dx.button(val, 0, 0, width, self.titleh)
-	--column.btn.onclick = function() self.sort(val) end
-
-	table.insert(self.columns, column)
-	return column
+function Gridlist:setColumnWidth(colIndex, width)
+	check('nn', {colIndex, width})
+	self.columns[colIndex].width = width
 end
 
-function Gridlist:removeColumn(i)
-	table.remove(self.columns, i)
-end
+function Gridlist:fitItemsToColumns()
+	if not self.autoSizeColumn then return end
+	for i=1, #self.columns do
+		local col = self.columns[i]
 
-function Gridlist:addItem(...)
-	local arg1, arg2 = unpack(arg)
-	local val = arg1
-	local imgColIndex = type(arg2) == 'number' and arg2
-	local callback = type(arg[#arg]) == 'function' and arg[#arg]
+		col.width = col.titleWidth + self.titleSpacing
 
-	local values = type(val) ~= "table" and {val} or val
-	if (imgColIndex) then
-		if not fileExists(values[imgColIndex]) then
-			error("file " .. values[imgColIndex] .. " doesn't exist", 2)
+		for j=1, #self.items do
+			local itemVal = self.items[j].values[i].width + self.titleSpacing
+			col.width = math.max(itemVal, col.width)
 		end
 	end
+end
 
-	if #self.columns == 0 then
-		return
-	end
+function Gridlist:addItem(values, onClick)
+	assert(#self.columns ~= 0, "the gridlist doesn't have any columns")
 
-	local item = {}
-	item.values = values
-	item.onClick = callback
-	item.imgColIndex = imgColIndex
+	local item = {
+		values = parseItemValues(self, values),
+		onClick = onClick or nil
+	}
 
 	table.insert(self.items, item)
-
 	updateRT(self)
-
 	return item
 end
 
-function Gridlist:removeItem(i)
-	table.remove(self.items, i)
-	
-	if #self.items == 0 then
-		self.selectedItem = 0
-	else
-		self.sp = math.max(1, self.sp - 1)
-	end
+function Gridlist:removeItem(itemIndex)
+	check('n', {itemIndex})
+	table.remove(self.items, itemIndex)
+	updateRT(self)	
+end
 
-	updateRT(self)
+function Gridlist:setItemValue(itemIndex, colIndex, value)
+	check('nns', {itemIndex, colIndex, value})
+	self.items[itemIndex].values[colIndex] = parseItemValues(self, value, true)
+end
+
+function Gridlist:getItemByIndex(itemIndex)
+	check('n', {itemIndex})
+	return self.items[itemIndex]
+end
+
+function Gridlist:getCount()
+	return #self.items
 end
 
 function Gridlist:clear()
@@ -99,33 +146,46 @@ function Gridlist:clear()
 	updateRT(self)
 end
 
-function Gridlist:sort(_type) -- this func needs to be redone, currently doesn't support sorting by column
-	local f = function(a, b) return a < b end
+function Gridlist:sort(reverse)
+	-- need to sort numbers first then strings?
+	local col = self.columns[1]
 
-	if (_type) then
-		f = function(a, b)
-			if (a.data[_type] and b.data[_type]) then
-				return a.data[_type] > b.data[_type]
-			else
-				error("can't sort by that key (not found)", 4)
+	if (col) then
+		table.sort(self.items, function(a, b)
+			if (reverse) then
+				return a.values[1] > b.values[1]
 			end
-		end
+			return a.values[1] < b.values[1]
+		end)
+		updateRT(self)
 	end
+end
 
-	table.sort(self.items, f)
-	updateRT(self)
+function Gridlist:sortByColumn(colIndex, reverse)
+	check('n', {colIndex})
+	local col = self.columns[colIndex]
+
+	if (col) then
+		table.sort(self.items, function(a, b)
+			if (reverse) then
+				return a.values[colIndex] > b.values[colIndex]
+			end
+			return a.values[colIndex] < b.values[colIndex]
+		end)
+		updateRT(self)
+	end
 end
 
 function Gridlist:draw()
 	if (not self.visible) then return end
 
 	self.mouseOver = mouseX and isMouseOverPos(self.x, self.y, self.w, self.h)
-	self.maxItems = math.floor((self.h-self.titleh)/(self.itemh+self.itemSpace))
+	self.maxItems = math.floor((self.h-self.titleh)/(self.itemh+self.itemSpacing))
 
 	dxDrawRectangle(self.x, self.y, self.w, self.h, tocolor(0,0,0,150))
 
-	if (not self.rt_updated or self.mouseOver) then
-		updateRT(self) -- draw items onto rt
+	if (not self.rt_updated or self.mouseOver and self.focused) then
+		updateRT(self) -- draw items onto the render target
 		self.rt_updated = true
 	end
 	
@@ -135,12 +195,14 @@ function Gridlist:draw()
 end
 
 function Gridlist:drawScrollBar()
-	if #self.items > self.maxItems then
+	if not self.scrollbarVisible then return end
+	local itemCount = #self.items
+	if itemCount > self.maxItems then
 		local shaftw = self.scrollbarWidth
 		local shafth = self.h
 
 		local thumbw = self.scrollbarWidth
-		local thumbh = (shafth/(#self.items-self.maxItems+1))
+		local thumbh = (shafth/(itemCount-self.maxItems+1))
 
 		local thumbPos = thumbh * (self.sp - 1)
 
@@ -153,69 +215,62 @@ end
 
 function Gridlist:drawItems()
 	if (not self.maxItems) then return end
-	self.ep = #self.items < self.maxItems and #self.items or self.sp + self.maxItems - 1
+	local itemCount = #self.items
+	self.ep = itemCount < self.maxItems and itemCount or self.sp + self.maxItems - 1
 
-	local xOff = 0
+	local paddingX = self.paddingLeft
+	local yOff = self.titleh
+	local xOff = paddingX
 
-	for j=1, #self.columns do
-		local yOff = self.titleh
-		local col = self.columns[j]
+	local columnCount = #self.columns
 
-		-- column title
-		if (self.titleh > 0) then
-			dxDrawText(col.val, xOff, 0, xOff + col.width, self.titleh, tocolor(255,255,220), 1.5, "default", "left", "center")
-		end
-
-		for i=self.sp, self.ep do
-			local item = self.items[i]
-			local val = item.values[j]
-
-			-- item click pos
-			item.pos = {
-				x = self.x,
-				y = self.y + yOff,
-				w = self.w - self.scrollbarWidth,
-				h = self.itemh
-			}
-
-			-- item styling
-			local style = self.style
-			local mo = self.focused and isMouseOverPos(item.pos.x, item.pos.y, item.pos.w, item.pos.h)
-			local textFont = "default"
-			local textSize = 1.25
-			local textColor = (self.selectedItem == i and tocolor(200,70,70)) or (mo and tocolor(55,155,55)) or item.color or tocolor(255,255,255)
-			local bgColor = (self.selectedItem == i and tocolor(33,33,33,222)) or (mo and tocolor(255,255,255,200)) or item.bgColor or tocolor(0,111,200,200)
-
-			-- item bg
-			if (j == 1) then
-				--dxDrawRectangle(0, yOff, self.w, self.itemh, bgColor)
-				dxDrawImage(xOff, yOff, item.pos.w, self.itemh, "./img/button.png", 0, 0, 0, tocolor(25,25,25))
-			end
-
-			if (val) then
-				-- item image
-				if (item.imgColIndex and item.imgColIndex == j) then
-					resizeColumn(col, self.itemh)
-
-					dxDrawImage(xOff, yOff, self.itemh, self.itemh, val, 0, 0, 0)
-				end
-
-				-- item text
-				if (item.imgColIndex ~= j) then
-					resizeColumn(col, val)
-
-					-- if (style.itemText.shadow) then
-					-- 	dxDrawText(val, xOff+1, yOff+1, xOff + col.width+1, yOff + self.itemh+1, tocolor(0, 0, 0, 255), textSize, textFont, "left", "center", true, false, false, false, false)
-					-- end
-				
-					dxDrawText(val, xOff, yOff, xOff + col.width, yOff + self.itemh, textColor, textSize, textFont, "left", "center", true, false, false, false, false)
-				end
-			end
-
-			yOff = yOff + self.itemh + self.itemSpace
-		end
+	-- column titles
+	for i=1, columnCount do
+		local col = self.columns[i]
 		
-		xOff = xOff + col.width + 10
+		if (self.titleh > 0) then
+			dxDrawText(col.value, xOff, 0, xOff + col.width, self.titleh, tocolor(255,255,220), self.textSize, "default-bold", "left", "center")
+		end
+
+		xOff = xOff + col.width
+	end
+
+	-- rows
+	for i=self.sp, self.ep do
+		local xOff = 0
+
+		local item = self.items[i]
+		item.pos = {
+			x = self.x,
+			y = self.y + yOff,
+			w = self.w - self.scrollbarWidth,
+			h = self.itemh
+		}
+
+		-- styles (temporary, eventually will be replaced with global styles)
+		local mo = self.focused and isMouseOverPos(item.pos.x, item.pos.y, item.pos.w, item.pos.h)
+		local textColor = (self.selectedItem == i and tocolor(255,255,255)) or (mo and tocolor(55,55,255,255)) or item.color or tocolor(255,255,255)
+		local bgColor = (self.selectedItem == i and tocolor(55,55,255,255)) or (mo and tocolor(15,15,15,255)) or item.bgColor or tocolor(23,23,23,255)
+
+		-- item background
+		dxDrawImage(xOff, yOff, item.pos.w, self.itemh, "./img/button.png", 0, 0, 0, bgColor)
+
+		for j=1, columnCount do
+			local col = self.columns[j]
+			local val = item.values[j].value
+
+			if val then
+				if col.checkThumbnails then
+					dxDrawImage(xOff, yOff, self.itemh, self.itemh, val, 0, 0, 0)
+				else
+					dxDrawText(val, xOff + paddingX, yOff, xOff + paddingX + col.width, yOff + self.itemh, textColor, self.textSize, 'arial', "left", "center", true, false, false, false, false)
+				end
+			end
+
+			xOff = xOff + col.width
+		end
+
+		yOff = yOff + self.itemh + self.itemSpacing
 	end
 end
 
@@ -240,7 +295,7 @@ function Gridlist:onKey(key, down)
 
 			if (self.mouseDown and isMouseOverPos(item.pos.x, item.pos.y, item.pos.w, item.pos.h)) then
 				self.selectedItem = i
-				if item.onClick then
+				if item.onClick and type(item.onClick) == 'function' then
 					item.onClick()
 				end
 			end
