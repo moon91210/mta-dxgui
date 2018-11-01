4a7986e866fff8380d305ff79d660c942bc713a4
moon91210 <smulders@protonmail.com>
2018-11-01 04:16:39 +0100

    rename property

--- components/window.lua
+++ components/window.lua
@@ -5,12 +5,12 @@ function Window.new(x, y, w, h, value)
 	local self = inherit(Component.new("window", x, y, w, h), Window)
 
 	self.value = value or ""
-	self.title_h = 30
-	self.offy = self.title_h
+	self.titleh = 30
+	self.offy = self.titleh
 	
-	local dragArea = DragArea(0, -self.offy, self.w, self.title_h):setParent(self)
+	local dragArea = DragArea(0, -self.offy, self.w, self.titleh):setParent(self)
 
-	self.closeBtn = Button.new(self.w - self.title_h, 3, self.title_h-3, self.title_h-5, "×")
+	self.closeBtn = Button.new(self.w - self.titleh, 3, self.titleh-3, self.titleh-5, "×")
 		:setParent(dragArea)
 		:on("click", function()
 			self.visible = false
@@ -24,11 +24,11 @@ function Window:draw()
 	local color = self.focused and tocolor(20,20,20,235) or tocolor(8,8,8,235)
 
 	-- titlebar
-	dxDrawRectangle(self.x, self.y, self.w, self.title_h, tocolor(0,0,0,245))
-	dxDrawText(self.value, self.x+2, self.y+2, self.x + self.w+2, self.y + self.title_h+2, tocolor(0,0,0,255), 1.5, "tahoma", "center", "center")
-	dxDrawText(self.value, self.x, self.y, self.x + self.w, self.y + self.title_h, tocolor(225,225,225,255), 1.5, "tahoma", "center", "center")
+	dxDrawRectangle(self.x, self.y, self.w, self.titleh, tocolor(0,0,0,245))
+	dxDrawText(self.value, self.x+2, self.y+2, self.x + self.w+2, self.y + self.titleh+2, tocolor(0,0,0,255), 1.5, "tahoma", "center", "center")
+	dxDrawText(self.value, self.x, self.y, self.x + self.w, self.y + self.titleh, tocolor(225,225,225,255), 1.5, "tahoma", "center", "center")
 	-- main bg
-	dxDrawRectangle(self.x, self.y + self.title_h, self.w, self.h - self.title_h, color)
+	dxDrawRectangle(self.x, self.y + self.titleh, self.w, self.h - self.titleh, color)
 end
 
 function Window:drawBorders()
