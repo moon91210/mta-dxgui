52f05b6a1a3c7b2020cd1c45759364bc30897164
moon91210 <smulders@protonmail.com>
2018-11-06 16:46:37 +0100

    changed to empty component, users can write their own draw function

--- components/pane.lua
+++ components/pane.lua
@@ -2,17 +2,7 @@ Pane = {}
 
 
 function Pane.new(x, y, w, h)
-	local self = inherit(Component.new('pane', x, y, w, h), Pane)
-	self.dragArea = DragArea(0, 0, self.w, self.h).setParent(self)
-	self.debug = false
-	return self
-end
-
-function Pane:draw()
-	dxDrawRectangle(self.x, self.y, self.w, self.h, tocolor(55,55,55,220))
-	if (self.debug) then
-		dxDrawText(math.ceil(self.x)..", "..math.ceil(self.y), self.x, self.y, self.w, self.h)
-	end
+	return inherit(Component.new('pane', x, y, w, h), Pane)
 end
 
 setmetatable(Pane, {__call = function(_, ...) return Pane.new(...) end})
