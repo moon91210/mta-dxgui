12d472e084776e5d51c8c6fd5c3f5f143f1cb7a5
moon91210 <smulders@protonmail.com>
2018-11-07 20:39:07 +0100

    add Slider component

---
+++ components/slider.lua
@@ -0,0 +1,70 @@
+Slider = {}
+local Seeker = {}
+
+
+function Slider.new(x, y, w, h)
+	local self = inherit(Component.new('Slider', x, y, w, h), Slider)
+	self.pos = 0
+	self.seekerWidth = 8
+	self.seeker = Seeker.new(0, 0, self.seekerWidth, h):setParent(self)
+	self.trackHeight = 10
+	return self
+end
+
+function Slider:draw()
+	local seeker = self.seeker
+
+	seeker.dragArea.dragging = mouseDown and (seeker.pos > 0 or mouseX > self.x) and (seeker.pos < 100 or mouseX < self.x + self.w)
+
+	dxDrawRectangle(self.x, self.y + (self.h-self.trackHeight)/2, self.w, self.trackHeight, tocolor(0,0,0,225))
+end
+
+function Slider:setSeekerPosition(pos)
+	check('n', {pos})
+	assert(pos >= 0 and pos <= 100, 'position must be a value between 0-100')
+	local seeker = self.seeker
+	self.seeker:setPosition(map(pos, 0, 100, 0, self.w - self.seekerWidth))
+	return self
+end
+
+function Slider:getSeekerPosition()
+	return self.seeker.pos
+end
+
+function Slider:setSeekerWidth(width)
+	check('n', {width})
+	self.seekerWidth = width
+	self.seeker.w = width
+	return self
+end
+
+function Slider:setTrackHeight(height)
+	check('n', {height})
+	self.trackHeight = height
+	return self
+end
+
+setmetatable(Slider, {__call = function(_, ...) return Slider.new(...) end})
+
+
+function Seeker.new(x, y, w, h)
+	local self = inherit(Component.new('Seeker', x, y, w, h), Seeker)
+	self:setDraggable(true)
+	self.pos = 0
+	return self
+end
+
+function Seeker:draw()
+	local x, y, w, h = self.x, self.y, self.w, self.h
+	local parent = self.parent
+
+	if parent then
+		local pos = map(x, parent.x, parent.x + parent.w - w, 0, 100)
+		if pos ~= self.pos then
+			self.pos = pos
+			self.parent:emit('change', pos)
+		end
+
+		dxDrawRectangle(x, y, w, h, tocolor(55,55,255,255), false, true)
+	end
+end
