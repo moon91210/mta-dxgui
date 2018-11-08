8a47f07549ec2ba56ed0c4bab957d2ab5637af65
moon91210 <smulders@protonmail.com>
2018-11-08 04:14:41 +0100

    minor edit

--- components/slider.lua
+++ components/slider.lua
@@ -12,16 +12,14 @@ end
 
 function Slider:draw()
 	local seeker = self.seeker
-
 	seeker.dragArea.dragging = mouseDown and (seeker.pos > 0 or mouseX > self.x) and (seeker.pos < 100 or mouseX < self.x + self.w)
-
+	-- draw the slider track
 	dxDrawRectangle(self.x, self.y + (self.h-self.trackHeight)/2, self.w, self.trackHeight, tocolor(0,0,0,225))
 end
 
 function Slider:setSeekerPosition(pos)
 	check('n', {pos})
 	assert(pos >= 0 and pos <= 100, 'position must be a value between 0-100')
-	local seeker = self.seeker
 	self.seeker:setPosition(map(pos, 0, 100, 0, self.w - self.seekerWidth))
 	return self
 end
