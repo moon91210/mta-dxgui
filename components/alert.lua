c5abdd20935f295e42b7fb0b0af1f82aa5932d0a
moon91210 <smulders@protonmail.com>
2019-08-11 14:52:13 +0200

    update Alert: add new parameter to lock background

--- components/alert.lua
+++ components/alert.lua
@@ -1,19 +1,28 @@
-function Alert(message, callback)
+function Alert(message, callback, lockBackground)
 	check('s', {message})
 
-	local self = Window(0, 0, 500, 280, 'Message')
-		:align('center')
-		:setOnTop()
-	
-	self:once('close', function()
-		self:destroy()
-	end)
+	local self
 
-	Label(130, 40, self.w*0.65, 30, message, 1.5, tocolor(255,255,255), true, 'sans', 'left', 'top')
-		:setParent(self)
+	local win = Window(0, 0, 500, 280, 'Alert')
+		:setOnTop()
+
+	if lockBackground then
+		self = Pane(0, 0, screenWidth, screenHeight)
+			:addChild(win)
+	else
+		self = win
+	end
+
+	win:align('center')
+	
+	win:once('close', function() self:destroy() end)
+	win:once('destroy', function() focus(2) end)
+
+	Label(130, 40, win.w*0.65, 30, message, 1.5, tocolor(255,255,255), true, 'sans', 'left', 'top')
+		:setParent(win)
 
 	Button(0, 170, 120, 40, 'OK')
-		:setParent(self)
+		:setParent(win)
 		:align('centerX')
 		:once('mouseup', function()
 			if type(callback) == 'function' then
@@ -23,7 +32,7 @@ function Alert(message, callback)
 		end)
 
 	Image(35, 40, 65, 65, './img/sign-info-icon.png')
-		:setParent(self)
+		:setParent(win)
 
 	return self
 end
